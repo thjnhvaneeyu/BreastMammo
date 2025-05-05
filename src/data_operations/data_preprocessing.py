404156f94b0c6169d238fd08430138164142edb4
Huynh Khoi Minh Uyen <uyenhuynhkhoiminh.1.1@gmail.com>
2025-05-06 00:39:01 +0700

    roi

--- src/data_operations/data_preprocessing.py
+++ src/data_operations/data_preprocessing.py
@@ -14,7 +14,6 @@ import cv2
 import re
 import config
 from data_operations.data_transformations import generate_image_transforms
-from data_operations.data_transformations import load_roi_and_label
 import xml.etree.ElementTree as ET
 from pydicom.errors import InvalidDicomError
 from sklearn.utils.class_weight import compute_class_weight
