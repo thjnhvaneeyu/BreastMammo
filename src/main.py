efbf243bf3c9ac52fcaa2a3432cb37acca941a95
Huynh Khoi Minh Uyen <uyenhuynhkhoiminh.1.1@gmail.com>
2025-05-07 04:50:40 +0700

    roi

--- src/main.py
+++ src/main.py
@@ -153,7 +153,7 @@ def main():
             X_test_list, y_test_list = [], []
             for img, lbl in ds_val.unbatch().as_numpy_iterator():
                 # chuyển Tensor → numpy
-                X_test_list.append(img.numpy())
+                X_test_list.append(img)
                 y_test_list.append(int(lbl))
             X_test = np.stack(X_test_list, axis=0)
             y_test = np.array(y_test_list)
